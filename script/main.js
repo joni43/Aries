766e68172ea3711281fdc49ce4f128391fe82afa
Stavros Tsirlidis <stavros.tsirlidis@student.kyh.se>
2018-11-18 14:48:07 +0100

    fixed so the title will hav three dots when its longer then 8 chars

--- script/main.js
+++ script/main.js
@@ -60,30 +60,51 @@ function createDocumentItem(title,dDate)
 
     document.getElementById("document-handler-container").appendChild(divDocumentHandlerItem);
 }
+
+//Make Title shorter
+function shortTitle(title)
+{
+    if (title.length > 8)
+        {
+            title = title.slice(0,8) + "..."; 
+        }
+    return title;
+}
+
 //Saves the document
 saveBtn.onclick = function()
 {
-        const key = inpTitleKey.value;
-        const value = "";
+    let key = inpTitleKey.value;
+    const value = "";
 
-        if(key){
-            localStorage.setItem(key,value);
-            createDocumentItem(key,today);
-        }
-        else{
-            alert("Ange dokument titel!");
-        }
+    key = shortTitle(key);
     
-        inpTitleKey.value ="";
-        inpDocumentText.value="";
-        location.reload();
+    if(key){
+        localStorage.setItem(key,value);
+        createDocumentItem(key,today);
+    }
+    else{
+        alert("Ange dokument titel!");
+    }
+    
+    inpTitleKey.value ="";
+    location.reload();
 }
 
+//New dokument clears the title and the text editor text
 newdocumentBtn.onclick = function()
 {
     inpTitleKey.value ="";
     location.reload(); 
 }
+
+//Deletes a saved document - Working on this one not finished (Stavros)
+function deleteDocument(documentTitle)
+{
+    localStorage.removeItem(documentTitle);
+}
+
+
 
 //loops the local storage 
 for (var i = 0; i < localStorage.length; i++)
