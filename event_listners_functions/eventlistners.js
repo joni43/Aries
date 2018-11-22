ba82a0786e4efe5ba48aca713b0f08d14f197cd8
Eddie Frodé Skogesjö <eddie.frodeskogesjo@outlook.com>
2018-11-22 14:48:02 +0100

    Local storage Mock added

--- event_listners_functions/eventlistners.js
+++ event_listners_functions/eventlistners.js
@@ -1,6 +1,7 @@
-
 const noteList = document.querySelector('#noteList');
 const noteForm = document.querySelector('#form');
+const localMockArray = mockLocalStorage();
+console.log(localMockArray)
 
 
 
@@ -46,16 +47,16 @@ window.onload = function () {
 }
 
 //Gets the date of today and formats it to mm/dd/yyyy , put new Date() in the funtion
-function today (date) {
+function today(date) {
     console.log(date);
     var dd = date.getDate();
-    var mm = date.getMonth() +1;
-    var yyyy= date.getFullYear();
-    
-    if (dd<10){
+    var mm = date.getMonth() + 1;
+    var yyyy = date.getFullYear();
+
+    if (dd < 10) {
         dd = '0' + dd;
     }
-    if (mm<10){
+    if (mm < 10) {
         mm = '0' + mm;
     }
     date = dd + '/' + mm + '/' + yyyy;
@@ -63,12 +64,10 @@ function today (date) {
 }
 
 //Makes Title shorter and adds ... in the end , use this one when the title is written over the document icon.
-function shortTitle(title)
-{
-    if (title.length > 8)
-        {
-            title = title.slice(0,8) + "..."; 
-        }
+function shortTitle(title) {
+    if (title.length > 8) {
+        title = title.slice(0, 8) + "...";
+    }
     return title;
 }
 
@@ -95,3 +94,57 @@ function submitForm() {
 
     return formValue
 };
+
+function mockLocalStorage() {
+
+    let mockLocalStorage = [{
+            id: 1,
+            noteTitle: 'Title 1',
+            noteBody: 'Body 1',
+            Favorite: true,
+            date: '25-08-1987'
+
+        },
+        {
+            id: 10,
+            noteTitle: 'Title 10',
+            noteBody: 'Body 10',
+            Favorite: false,
+            date: '25-08-1987'
+
+        },
+        {
+            id: 5,
+            noteTitle: 'Title 5',
+            noteBody: 'Body 4',
+            Favorite: false,
+            date: '25-08-1987'
+
+        },
+        {
+            id: 32,
+            noteTitle: 'Title 32',
+            noteBody: 'Body 32',
+            Favorite: true,
+            date: '25-08-1987'
+
+        },
+        {
+            id: 1000,
+            noteTitle: 'Title 1000',
+            noteBody: 'Body 1000',
+            Favorite: true,
+            date: '25-08-1987'
+
+        }, {
+            id: 2,
+            noteTitle: 'Title 2',
+            noteBody: 'Body 2',
+            Favorite: true,
+            date: '25-08-1987'
+
+        }
+    ]
+    return mockLocalStorage;
+
+}
