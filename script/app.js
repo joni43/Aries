b886e1e288cf4e8973b0496f5576599e4dc5bdc4
Stavros Tsirlidis <stavros.tsirlidis@student.kyh.se>
2018-11-22 23:21:57 +0100

    cleaned up a little bit

--- script/app.js
+++ script/app.js
@@ -9,84 +9,84 @@ const newdocumentBtn = document.getElementById("new-document-btn");
 //Variables
 const inputTitle = document.getElementById("inputTitle");
 
-//Gets the date of today and formats it
-function today (date){
-    console.log(date);
-    var dd = date.getDate();
-    var mm = date.getMonth() +1;
-    var yyyy= date.getFullYear();
-    
-    if (dd<10){
-        dd = '0' + dd;
-    }
-    if (mm<10){
-        mm = '0' + mm;
-    }
-    date = dd + '/' + mm + '/' + yyyy;
-    return date;
-}
-
-
-
-
-//object constructor
-
-
 
 //Functions
 
-//Makes Title shorter and adds ...
-function shortTitle(title)
-{
-    if (title.length > 8)
-        {
-            title = title.slice(0,8) + "..."; 
+ //Makes Title shorter and adds ...
+    function shortTitle(title)
+    {
+        if (title.length > 8)
+            {
+                title = title.slice(0,8) + "..."; 
+            }
+        return title;
+    }
+
+    //Gets the date of today and formats it
+    function today (date){
+        console.log(date);
+        var dd = date.getDate();
+        var mm = date.getMonth() +1;
+        var yyyy= date.getFullYear();
+        
+        if (dd<10){
+            dd = '0' + dd;
         }
-    return title;
-}
+        if (mm<10){
+            mm = '0' + mm;
+        }
+        date = dd + '/' + mm + '/' + yyyy;
+        return date;
+    }
 
  //Creates a document item
-function createDocumentItem(title,dDate)
-{
+    function createDocumentItem(title,dDate)
+    {
 
-    let divDocumentHandlerItem = document.createElement('div');
-    let divDocumentImage = document.createElement('div');
-    let divDocumentTitle = document.createElement('div');
-    let divDocumentTime = document.createElement('div');
-    let documentTitle = document.createTextNode(title);
-    let documentTime = document.createTextNode(dDate);
-    let iconTrashCan = document.createElement('i');
-    let iconStar = document.createElement('i');
-    
+        let divDocumentHandlerItem = document.createElement('div');
+        let divDocumentImage = document.createElement('div');
+        let divDocumentTitle = document.createElement('div');
+        let divDocumentTime = document.createElement('div');
+        let documentTitle = document.createTextNode(title);
+        let documentTime = document.createTextNode(dDate);
+        let iconTrashCan = document.createElement('i');
+        let iconStar = document.createElement('i');
+        
 
-    divDocumentHandlerItem.className="document-handler-item";
-    divDocumentTitle.className="item-title";
-    divDocumentTime.className="item-time";
-    divDocumentImage.className ="img-document";
-    iconStar.className ="fas fa-star";
-    iconTrashCan.className="fas fa-trash-alt";
-    
-    divDocumentTitle.appendChild(documentTitle);
-    divDocumentTime.appendChild(documentTime);
+        divDocumentHandlerItem.className="document-handler-item";
+        divDocumentTitle.className="item-title";
+        divDocumentTime.className="item-time";
+        divDocumentImage.className ="img-document";
+        iconStar.className ="fas fa-star";
+        iconTrashCan.className="fas fa-trash-alt";
+        
+        divDocumentTitle.appendChild(documentTitle);
+        divDocumentTime.appendChild(documentTime);
 
-    divDocumentHandlerItem.appendChild(divDocumentTitle);
-    divDocumentHandlerItem.appendChild(iconStar);
-    divDocumentHandlerItem.appendChild(divDocumentImage);
+        divDocumentHandlerItem.appendChild(divDocumentTitle);
+        divDocumentHandlerItem.appendChild(iconStar);
+        divDocumentHandlerItem.appendChild(divDocumentImage);
 
-    divDocumentHandlerItem.appendChild(divDocumentTime);
-    divDocumentHandlerItem.appendChild(iconTrashCan);
+        divDocumentHandlerItem.appendChild(divDocumentTime);
+        divDocumentHandlerItem.appendChild(iconTrashCan);
 
-    document.getElementById("document-handler-container").appendChild(divDocumentHandlerItem);
+        document.getElementById("document-handler-container").appendChild(divDocumentHandlerItem);
     }   
 
     function submitForm() {
-    let formValue = {};
-    formValue.id = "nothing right now";
-    formValue.inputTitleValue = inputTitle.value;
-    formValue.textaareaValue = quill.getContents(); //here we get the content from the editor!
-    formValue.dateValue = today(new Date);
-    formValue.favorite = "nothing right now";
-    return formValue
+        let formValue = {};
+        formValue.id = "nothing right now";
+        formValue.inputTitleValue = inputTitle.value;
+        formValue.textaareaValue = quill.getContents(); //here we get the content from the editor!
+        formValue.dateValue = today(new Date);
+        formValue.favorite = "nothing right now";
+        return formValue
+    };
+
+    //clears the form 
+    function clearForm(){
+        inputTitle.value ="";
+        location.reload();
     };
 
     //Saves the document
@@ -103,17 +103,12 @@ function createDocumentItem(title,dDate)
         alert("Ange dokument titel!");
     }
     
-    inputTitle.value ="";
-    location.reload();
+    clearForm();
+    
     }
 
     //New dokument clears the title and the text editor text
-    newdocumentBtn.onclick = function()
-    {
-        inputTitle.value ="";
-        location.reload(); 
-    }
-
+    newdocumentBtn.addEventListener("click",clearForm);
 
     //loops the local storage --this loop is not finished
     for (var i = 0; i < localStorage.length; i++)
@@ -124,65 +119,3 @@ function createDocumentItem(title,dDate)
 
 } 
 
-
-
-
-
-
-
-
-
-/*
-//Laboration Area//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////
-//Deletes a saved document - Working on this one not finished (Stavros)
-    const newdocumentBtn = document.getElementById("new-document-btn");
-    newdocumentBtn.onclick = function()
-    {
-        
-        let documents = loadDocuments();
-        documents.push(newDocument(getAvailID(documents)));
-        saveDocuments(documents);
-    } 
-    
-}
-function loadDocuments()
-{
-    return JSON.parse(localStorage.getItem('documents'));
-}
-function getAvailID(doc)
-{
-    if (doc.length > 0)
-    {
-        return (Math.max(...doc.map(d => d.id),0) + 1 );
-    }
-    else
-    {
-        return 0;
-    }
-}
-function saveDocuments(doc)
-{
-    localStorage.setItem('documents',JSON.stringify(doc));
-}
-function updateView()
-{
-}
-function deleteDocument()
-{
-    localStorage.setItem('documents',JSON.stringify([]));
-}
-function newDocument ()
-{
-    let newDocument = {};
-    newDocument.title = 'ewe';
-    newDocument.content = 'wee';
-    newDocument.date = 'wewe';
-    newDocument.favorite = 'wewe';
-    return newDocument;
-}
-//Laboration Area//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////
-*/
