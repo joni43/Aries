526fa4bc13b03d48de3d853a165c89a9a8dd2b20
madelennie <nordlund.jennie@gmail.com>
2018-11-30 21:32:25 +0100

    some changes added

--- script/index.js
+++ script/index.js
@@ -26,3 +26,8 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function redirectButton () {
+  var button = dokument.getElementById("button");
+    
+}
