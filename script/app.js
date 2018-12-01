398c136918ae2f259d34a6f91c387c39b7a2599a
Stavros Tsirlidis <stivve@hotmail.com>
2018-12-01 21:56:32 +0100

    try to fix the new note button

--- script/app.js
+++ script/app.js
@@ -51,6 +51,7 @@
 
     //Saves new document if noteToView is true it updates existing note
     saveBtn.onclick = function () {
+        console.log(noteToView);
         if (noteToView) {
             updateNote(noteToView);
         } else {
@@ -203,8 +204,11 @@
    //Saves the note 
    function saveNote() {
        let newNote = {};
-
-       newNote.id = createID();
+       let newNoteID = createID();
+       console.log(newNoteID);
+       noteToView = newNoteID;
+       console.log(noteToView);
+       newNote.id = newNoteID;
        newNote.title = inputTitle.value;
        newNote.textContent = quill.getContents(); //here we get the content from the editor!
        newNote.date = today(new Date);
@@ -225,6 +229,8 @@
    //clears the form 
    function clearForm() {
        inputTitle.value = "";
+       quill.setContents([]);
+       noteToView = "";
        updateView();
    }
 
