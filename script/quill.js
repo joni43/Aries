31d9cff5696eddd313ab2f7646ec79a728ae6cde
Stavros Tsirlidis <stavros.tsirlidis@student.kyh.se>
2018-12-04 14:32:13 +0100

    accepted

--- script/quill.js
+++ script/quill.js
@@ -4,24 +4,13 @@
 // Variabeln toolbarOptions används i quill-konstruktorn
 // Anger vad som ska ingå i tool-bar 
 // Tool-bar funktionaliteten finns redan i quills biblotek.
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 85d63a3fcc0c93dddbd0ee724f579c4634c85c02
 
 let toolbarOptions = [
   
   ['bold', 'italic', 'underline', 'strike' ],
   [{ 'size': ['small', false, 'large', 'huge', 'h1'] }, ],
   [ 'link', 'blockquote', 'code-block', 'image' ],
-<<<<<<< HEAD
-  [{ 'color': [] }, { 'background': [] }, { list: 'ordered' }, { list: 'bullet' } ],
-  
-=======
   [{ 'color': [] }, { 'background': [] }, { list: 'ordered' }, { list: 'bullet' } ]
->>>>>>> 85d63a3fcc0c93dddbd0ee724f579c4634c85c02
 ];
 
 // Konstruktor, skapar ett objekt av klassen Quill
